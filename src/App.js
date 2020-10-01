e5999c1d175eb605bd568dd94222c8197b6335de
Mubeen <fiskerdannis@gmail.com>
2020-10-01 16:00:33 +0500

    Messages from users are receiving locally, hooks are used

--- src/App.js
+++ src/App.js
@@ -1,29 +1,44 @@
 import React from 'react';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import './App.css';
+import {Button, FormControl, InputLabel, Input} from '@material-ui/core';
+import Message from './Message';
 
 function App() {
   const [input, setInput]= useState("");
-  const [messages, setMessages]= useState(['hello','hi']);
-  
+  const [messages, setMessages]= useState([
+    {username: "Mubeen", text: "Hello"},
+    {username:"Midha", text:"Hi"}
+  ]);
+  const [username, setUsername]= useState("");
+  useEffect(() => {
+    setUsername(prompt("Please enter your name"))
+  }, [])
+
+
   const sendMessages=(event) =>{
     event.preventDefault();
-      setMessages([...messages, input]);
+      setMessages([...messages, {username: username, text: input}]);
       setInput('');
   }
   
   return (
     <div className="App">
        <h1>Messenger app clone</h1>
-      <form>
+       <h2>Welcome {username}</h2>
+      <form> 
+        <FormControl>
+
+          <InputLabel >Enter message here</InputLabel>
+          <Input value={input} onChange={(Event) =>setInput(Event.target.value)}/>
+          <Button disabled={!input} variant="contained" color="primary" type="submit" onClick={sendMessages}>Send</Button>
+        </FormControl>
         
-      <input value={input} onChange={(Event) =>setInput(Event.target.value)}/>
-      <button type="submit" onClick={sendMessages}>Send</button>
       </form>
-      
- 
+
       {messages.map(messege=>(
-        <p>{messege}</p>
+        <Message username={messege.username} text={messege.text}/>
+
       ))
       }
     </div>
